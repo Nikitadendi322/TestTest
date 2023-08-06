7d506569121223740927f9cb4abcf500604dc7de
Никита <Ployka322@yandex.ru>
2023-08-06 10:29:57 +0700

    Start Test

---
+++ src/main/java/com/example/testwork2/TestWork2Application.java
@@ -0,0 +1,13 @@
+package com.example.testwork2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWork2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWork2Application.class, args);
+    }
+
+}
